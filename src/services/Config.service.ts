78a210996923041dce21fecc477e21bfd5e6a3c9
Edixon Piña <edixonalbertto@gmail.com>
2023-03-08 10:41:48 -0400

    move type to service config

--- src/services/Config.service.ts
+++ src/services/Config.service.ts
@@ -1,5 +1,13 @@
 import { load } from 'dotenv'
-import { TConfig } from '$/typing.ts'
+
+type TConfig =
+	| 'URL_API_DISCORD'
+	| 'URL_WS_DISCORD'
+	| 'VERSION_DISCORD'
+	| 'TOKEN_BOT'
+	| 'ID_SERVER'
+	| 'ID_CHANNEL_LISTENING_LIST'
+	| 'ID_CHANNEL_RESPONSE'
 
 export class ConfigService {
 	private static envs: Record<string, string>
@@ -14,6 +22,6 @@ export class ConfigService {
 
 	public getObject(): Record<TConfig, string> {
 		const envsObject = ConfigService.envs || Deno.env.toObject()
-		return envsObject as unknown as Record<TConfig, string>
+		return envsObject
 	}
 }
